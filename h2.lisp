ad2433f50afadbbab8bd669d34ef3dc8438f4350
Renzo <rtejada@gmu.edu>
2016-02-11 17:27:25 -0500

    some progress
    na

--- h2.lisp
+++ h2.lisp
@@ -23,94 +23,140 @@
      	((evenp n) nil)
      	(t 
        	 (loop for i from 3 to (isqrt n) by 2
-             never (zerop (mod n i))))))
+             never (zerop (mod n i)))))
+)
+
+
+(defun nub-helper (xs r_list)
+	cond (
+		if xs
+			(nub-helper (cdr xs) (cons (car xs) r_list))	
+		(null xs)
+				(r_list)
+		)
+)
+				 ;( nub-helper ((cdr xs) r_list)))
+
+	;(t(r_list))
+
+
+(defun nub (xs)
+ 	 (nub-helper xs 0)
+)
+ ;(nub '(1 2 3 4 5 6))
+; 
 
 
 
-(defun list-reverse (L)
-  "Create a new list containing the elements of L in reversed order."
-  (list-reverse-aux L nil))
-
-(defun list-reverse-aux (L A)
-  "Append list A to the reversal of list L."
-  (if (null L)
-      A
-    (list-reverse-aux (rest L) (cons (first L) A)
-    )
-  ) 
- )
-
-
-(defparameter *age* 19) ; Create variable age
-
-(defvar *college-ready* nil)
  
 ;;
 
-(defvar lista '(1 1 1 2 2 3 4 4 4 4 4))
-( if(not(member 5 lista))  (print "success"))
-;; (nub '(1 2 3 4 5 6))
+; (defvar lista '(1 1 1 2 2 3 4 4 4 4 4))
+; ( if(not(member 5 lista))  (print "success"))
+;(nub '(1 2 3 4 5 6))
+
+
+; ; (defun nub-helper (xs r_list)
+; ; 	(if (not(null xs))
+; ; 				(cons (car xs) r_list)
+; ; 				(nub-helper ((cdr xs) r_list))
+; ; 	)
+; ; 	(t(r_list))
+; ; )
+
+
+; (defun nub (xs)
+; 	; (nub-helper (xs nil))
+; )
+; 	; (cond  
+;  ;  ; 1st case: r_list is empty
+;  ;        ( 
+;  ;          (null r_list)       
+; 	;         (format t "~%Head of xs pushed to r_list ~%")
+;  ;            ( cons (car xs) r_list)
+; 	; 		(nub (cdr xs) r_list)    
+;  ;        )
+; 	; 	  ;;2nd Case: if element in the head of xs is is not in list
+; 	; 	  (
+; 	; 	  	 ; (member 1 '(1 2 3)) ;;
+; 	; 	  			(format t "Not a member then add~%")
+; 	; 	  			;(push (car xs) r_list)
+; 	; 	  			;(nub (cdr xs) r_list)
+;  ;        )
+; 	; 	   ;;pr
+; 	; 	  ;(
+; 	; 	  ; 	(not (null? x))
+; 	; 	  ; 		(nub (cdr xs) r_list)
+; 	; 	  ; );; cae element is in result list but there is elements left in cx
+; 	; 	  ; ( (t)(r_list))
+; 	;)
+; )
+
+; ; (nub '(1 2 3 4 5 6))
+
+; (defvar xs '(1 2 3 4))
+; (defvar r_list '(1 2))
+
+; (if(member 1 r_list)(format t "~%Head of xs pushed to r_listsssssaaa ~%"));;
+; (format t "~%Head of xs pushed trfsdafasfdsa ~%");;
 
 
 
-(defun nub (xs &optional (r_list '()))
-	
-	(cond 
-  ; 1st case: r_list is empty
-        ( 
-          (null r_list)       
-			       (format t "~%Head of xs pushed to r_list ~%")
-             (push (car xs) r_list)
-			       (nub (cdr xs) r_list)    
-        )
-		  ;;2nd Case: if element in the head of xs is is not in list
-		  ; (
-		  ; 	if(not(member (car xs) r_list));;
-		  ; 		(progn
-		  ; 			(format t "Not a member then add~%")
-		  ; 			(push (car xs) r_list)
-		  ; 			(nub (cdr xs) r_list)
-		  ; 		)
-		  ;  );;pr
-		  ; (
-		  ; 	(not (null? x))
-		  ; 		(nub (cdr xs) r_list)
-		  ; );; cae element is in result list but there is elements left in cx
-		  ; ( (t)(r_list))
-	)
-)
-
-(nub '(1 2 3 4 5 6))
-
-(defun max3 (x y z)
-  "chooses biggest of three"
-  (if (and (>= x y) (>= x z)) 
-  		x
-    (if (>= y z) y
-    	 z )
-   )
- )
 
 
-(cond 
-      (
-       (>= *age* 18) ; If T do this
-		      (setf *college-ready* 'yes)
-		      (format t "Ready for College ~%")
-      )
 
-  	  (
-       (< *age* 18) ; Else If T do this
-  		    (setf *college-ready* 'no)
-		      (format t "Not Ready for College ~%")
-      )
+
+
+
+
+
+
+
+
+
+
+; ; (defun max3 (x y z)
+; ;   "chooses biggest of three"
+; ;   (if (and (>= x y) (>= x z)) 
+; ;   		x
+; ;     (if (>= y z) y
+; ;     	 z )
+; ;    )
+; ;  )
+
+
+; ; (cond 
+; ;       (
+; ;        (>= *age* 18) ; If T do this
+; ; 		      (setf *college-ready* 'yes)
+; ; 		      (format t "Ready for College ~%")
+; ;       )
+
+; ;   	  (
+; ;        (< *age* 18) ; Else If T do this
+; ;   		    (setf *college-ready* 'no)
+; ; 		      (format t "Not Ready for College ~%")
+; ;       )
   	  
-      (
-        t 
-          (format t "Don't Know ~%")
-      )
-) 
+; ;       (
+; ;         t 
+; ;           (format t "Don't Know ~%")
+; ;       )
+; ; ) 
 
+
+; ;(defun list-reverse (L)
+; ;   "Create a new list containing the elements of L in reversed order."
+; ;   (list-reverse-aux L nil))
+
+; ; (defun list-reverse-aux (L A)
+; ;   "Append list A to the reversal of list L."
+; ;   (if (null L)
+; ;       A
+; ;     (list-reverse-aux (rest L) (cons (first L) A)
+; ;     )
+; ;   ) 
+; ;  )
 
 
 
